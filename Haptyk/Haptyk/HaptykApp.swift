233e069156d819f82bda98a50208ac950e6e6d93
parsamivehchi <parsa@mivehchi.net>
2020-11-15 16:11:49 -0800

    created xcode project
    making a iOS app called Haptyk, a basic app that lets the user feel different patterns using the Haptic Engine on the iPhone

---
+++ Haptyk/Haptyk/HaptykApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaptykApp.swift
+//  Haptyk
+//
+//  Created by Parsa Mivehchi on 2020-11-15.
+//
+
+import SwiftUI
+
+@main
+struct HaptykApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
